3e3012431e742016e60a83deb2745246ddc78410
Louie D <louie.delrosario97@gmail.com>
2026-04-21 13:24:58 -0700

    Added the line number in the while loop. We added a variable linenUmber and set it to 1. and looped lineNumber++ so it increased each line

--- src/Story.java
+++ src/Story.java
@@ -5,14 +5,14 @@ public class Story {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Add Scanner class
 
-        System.out.println(" ");                               // Menu (Visual)
+        System.out.println();                               // Menu (Visual)
         System.out.println("       Story Select");
         System.out.println("--------------------------");
         System.out.println("(1) Goldilocks");
         System.out.println("(2) Hansel and Gretel")    ;
         System.out.println("(3) Mary Had a Little Lamb");
-        System.out.println(" ");
-        System.out.println("Select a Story (Enter number): ");  // Prompt user for story input
+        System.out.println();
+        System.out.println("Select a Story (Enter number): "); // Prompt user for story input
         int storySelect = scanner.nextInt();                    // Scans users input
 
         //add in switch. First time using switches, found out you have to
@@ -33,13 +33,15 @@ public class Story {
                 break;
         }
         try {
-            FileReader fileReader = new FileReader(fileTitle); // FileReader connects the file we call in the Scanner. We plug in 'fileTitle' from the switch block setup as the file path we select.
+            FileReader fileReader = new FileReader(fileTitle);               // FileReader connects the file we call in the Scanner. We plug in 'fileTitle' from the switch block setup as the file path we select.
             BufferedReader bufferedReader = new BufferedReader(fileReader); // BufferedReader efficiently displays the information
-            // the FileReader connects to... (I think)
+                                                                           // the FileReader connects to... (I think)
 
-            String input; // We declare this string. It will be the vehicle where we will display the text of the file.
-            while ((input = bufferedReader.readLine()) != null) { // input will display the buffered readers output
-                System.out.println(input);                      // one line at a time (.readLine()) and will continue as long as != null is true. Meaning it will continue to read until the next line IS empty.
+            String input;
+            int lineNumber = 1;                                              // We declare this string. It will be the vehicle where we will display the text of the file.
+            while ((input = bufferedReader.readLine()) != null) {                  // input will display the buffered readers output
+                System.out.println(lineNumber+  ". " + input);
+                lineNumber++;                                                    // one line at a time (.readLine()) and will continue as long as != null is true. Meaning it will continue to read until the next line IS empty.
             }
         } catch (
                 IOException e) { // this will catch runtime exceptions, so the code can still run, and we can display certain outputs if an error occurs.
